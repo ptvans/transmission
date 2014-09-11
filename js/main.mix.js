3d2d580c9ab0cfc8c26431b2663aa001e64ba5bf
Michael Porath <michael@porath.ch>
2014-09-10 20:52:04 -0700

    calculator

--- js/main.mix.js
+++ js/main.mix.js
@@ -1,2 +1,3 @@
 @import "src/low-hanging-fruit.js";
+@import "src/policy-scenario.js";
 @import "src/app.js";
--- js/src/app.js
+++ js/src/app.js
@@ -3,5 +3,21 @@ var Model = Backbone.Model.extend({});
 $(document).ready(function() {
 	var lowHangingFruitView = new LowHangingFruitView({ el: "#low-hanging-fruit" });
 	lowHangingFruitView.render();
+
+	d3.json("../data/scenarios.json", function(data) {
+		var dataTaxes = data.taxes;
+		var dataInnovation = data.innovation;
+		var dataCountries = data.countries;
+
+		var taxesScenarioView = new PolicyScenario({ "model": new Model(), "data": dataTaxes, el: "#scenario-taxes-cost"});
+		taxesScenarioView.render();
+
+		var innovationScenarioView = new PolicyScenario({ "model": new Model(), "data": dataInnovation, el: "#scenario-innovation"});
+		innovationScenarioView.render();
+
+		var countryScenarioView = new PolicyScenarioCountries({ "model": new Model({ index: 0 }), "data": dataCountries, el: "#scenario-taxes-consumer-benefit"});
+		countryScenarioView.render();
+
+	});
 });
  
